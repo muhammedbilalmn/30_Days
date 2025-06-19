e9a2cefefe837241c3ab0f0838c7bdbc85603b0c
Muhammed Bilal M N <muhammedbilalmn585@gmail.com>
2025-06-19 16:06:07 +0530

    added otheer files and aniverse project

---
+++ 8day/practicejava.js
@@ -0,0 +1,31 @@
+//console.log("hello Bilal")
+var a=5;
+var b=3
+var sum=a+b;
+console.log(sum);
+const pi=3.14;
+console.log(pi);
+{
+    let d=10
+    console.log(d);
+}
+
+function fn()
+{
+    console.log("function executed");
+    
+}
+
+fn();
+
+for(let i=0;i<5;i++)
+{
+    console.log("iteration"+i)
+}
+
+let fruits = ["apple","Banana","orange"];
+fruits.push("grape");
+console.log(fruits)
+
+let person = {name:"ramu",age:30,dep:"CS"}
+console.log(person)
