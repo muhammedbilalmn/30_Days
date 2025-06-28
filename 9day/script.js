a49c9860f9677c07b839d092f7a148365e357e08
Muhammed Bilal M N <muhammedbilalmn585@gmail.com>
2025-06-28 11:28:38 +0530

    weather app

---
+++ 9day/script.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", function () {
+    const splash = document.getElementById("splashScreen");
+    const main = document.getElementById("mainContent");
+
+    splash.style.opacity = 0;
+
+    setTimeout(() => {
+        splash.style.display = "none";
+        main.style.display = "block";
+    }, 500); // Wait for transition
+});
+
+const apiKey = "4431de4ca47b08e5fc93517e8150272e"
+
+async function getWeather() {
+    
+    const city = document.getElementById('cityInput').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+                
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        displayWeather(data);
+
+    } catch (error) {
+        console.error('failed to fetch weather data',error);
+        alert('failed to fetch weather data ');
+        
+    }
+}
+
+function displayWeather(data){
+    // destructuring the data
+    const {main: {temp, humidity}, weather, wind:{speed}, sys:{country},name} = data;
+    const {main: weatherMain, description, icon} = weather[0];
+    // DOM manipulation
+    const weatherDisplay = document.getElementById('weatherDisplay');
+    if(data.cod !== 200){
+        weatherDisplay.innerHTML = `<p>Error: ${data.message}</P>`
+    return;
+    }
+
+    const weatherHTML = `
+        <h2>Weather in ${name}, ${country}</h2>
+        <p> temperature: ${temp}</p>
+        <p> Weather: ${weatherMain} (${description})</p>
+        <p> Humidity: ${humidity}</p>
+        <p> wind speed: ${speed}</p>
+        <img src="https://openweathermap.org/img/w/${icon}.png" alt="weather icon">
+
+    `;
+    weatherDisplay.innerHTML = weatherHTML
+}
+
